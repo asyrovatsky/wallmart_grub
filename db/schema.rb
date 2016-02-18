8878cda9c6d7d9793285f571965179bc9541a8b1
Alexandr Syrovatsky <asyrovatsky@brightgrove.com>
2016-02-18 15:01:37 +0200

    Prepare for heroku deploy

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160217163456) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "searches", force: :cascade do |t|
     t.string   "product_id"
     t.text     "text"
